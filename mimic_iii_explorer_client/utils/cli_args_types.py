ecb462ee2d7813d880f867c2f3af9d8d5062fea3
Jernej Vivod <jernej.vivod@medius.si>
2023-01-29 11:52:58 +0100

    [fix][#33] Fix argument parsing for test set size, augment sample/predefined run configurations

--- mimic_iii_explorer_client/utils/cli_args_types.py
+++ mimic_iii_explorer_client/utils/cli_args_types.py
@@ -8,7 +8,7 @@ def dir_path(path: str) -> str:
     return path
 
 
-def train_size(value):
+def test_size(value):
     value = float(value)
     if value < 0.0 or value > 1.0:
         raise argparse.ArgumentTypeError("Train set decimal fraction size should be between 0.0 and 1.0.")
