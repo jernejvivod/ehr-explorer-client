44e0f413fe88e632903faa680e036380687a073b
Jernej Vivod <vivod.jernej@gmail.com>
2023-02-18 15:59:32 +0100

    [feat][#44] Add run config example for computing Wordification (basic example)

---
+++ run_config_examples/compute_wordification/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# define the path to the script
+script_path=$(dirname "$0")
+
+# define paths to the configuration files
+ids_spec_path="$script_path/config/ids_spec.json"
+wordification_config_path="$script_path/config/wordification_config.json"
+target_spec_path="$script_path/config/target_spec.json"
+
+# define the output directory
+output_dir="$script_path/results"
+
+# run the command
+python3 "$script_path/../../mimic_iii_explorer_client" \
+    compute-wordification \
+    --ids-spec-path "$ids_spec_path" \
+    --wordification-config-path "$wordification_config_path" \
+    --target-spec-path "$target_spec_path" \
+    --limit-ids 0.01 \
+    --test-size 0.2 \
+    --output-dir "$output_dir"
